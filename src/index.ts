9de43c84d090aca5b4bc4af13a39009b2781262a
shane garrity <shanedgarrity@gmail.com>
2019-07-21 15:41:31 -0400

    stream input file to stdout

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-console.log('hello world');
+import * as fs from 'fs';
 
+function main() {
+  const path = 'input.txt';
+  const options = {
+    encoding: 'UTF-8',
+  };
+
+  const stream = fs.createReadStream(path, options);
+  stream.on('data', (chunk) => {
+    console.log(`${chunk}\nlength: ${chunk.length}`);
+  });
+
+  stream.on('end', () => {
+    console.log('over');
+  });
+}
+
+main();
