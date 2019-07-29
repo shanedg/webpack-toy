e72d48bfa62e2bf9025061dbf3d2b8ec1bd6ae87
shane garrity <shanedgarrity@gmail.com>
2019-07-29 00:09:20 -0400

    Convert webpack config to export a function
    Exporting the webpack config as a function allows us to conditionally
    set config options based on environment and options passed to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -16,109 +16,111 @@ const webpack = require('webpack');
  *
  */
 
-module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.ts'
-    },
+module.exports = function(env, args) {
+    return {
+        mode: 'development',
+        entry: {
+            index: './src/index.ts'
+        },
 
-    output: {
-        filename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
+        output: {
+            filename: '[name].[chunkhash].js',
+            path: path.resolve(__dirname, 'dist')
+        },
 
-    plugins: [
-        new webpack.ProgressPlugin(),
-        // new BundleAnalyzerPlugin({
-        //     analyzerMode: 'static',
-        //     analyzerHost: '127.0.0.1',
-        //     analyzerPort: '8888',
-        //     reportFilename: '../stats/config-report.html',
-        //     defaultSizes: 'parsed',
-        //     // openAnalyzer: true,
-        //     openAnalyzer: false,
-        //     generateStatsFile: true, // TODO: what if this was true, tho?
-        //     statsFilename: '../stats/config-stats.json',
-        //     // statsOptions: null, // TODO: a bunch of config options available here
-        //     statsOptions: {
-        //         // all: true,
-        //         assets: true,
-        //         assetsSort: '',
-        //         builtAt: true,
-        //         cached: true,
-        //         cachedAssets: true,
-        //         children: true,
-        //         chunkModules: true,
-        //         chunkOrigins: true,
-        //         chunks: true,
-        //         chunksSort: true,
-        //         // context: ''
-        //         depth: true,
-        //         entrypoints: true,
-        //         env: true,
-        //         errorDetails: true,
-        //         errors: true,
-        //         // exclude: '',
-        //         // excludeAssets: true,
-        //         // excludeModules: true,
-        //         hash: true,
-        //         // maxModules: 0,
-        //         moduleTrace: true,
-        //         modules: true,
-        //         // modulesSort: '',
-        //         optimizationBailout: true,
-        //         performance: true,
-        //         providedExports: true,
-        //         // publicPath: '',
-        //         reasons: true,
-        //         source: true,
-        //         timings: true,
-        //         usedExports: true,
-        //         version: true,
-        //         warnings: true,
-        //         // warningsFilter: '',
-        //     },
-        //     excludeAssets: null,
-        //     logLevel: 'info',
-        // }),
-    ],
+        plugins: [
+            new webpack.ProgressPlugin(),
+            // new BundleAnalyzerPlugin({
+            //     analyzerMode: 'static',
+            //     analyzerHost: '127.0.0.1',
+            //     analyzerPort: '8888',
+            //     reportFilename: '../stats/config-report.html',
+            //     defaultSizes: 'parsed',
+            //     // openAnalyzer: true,
+            //     openAnalyzer: false,
+            //     generateStatsFile: true, // TODO: what if this was true, tho?
+            //     statsFilename: '../stats/config-stats.json',
+            //     // statsOptions: null, // TODO: a bunch of config options available here
+            //     statsOptions: {
+            //         // all: true,
+            //         assets: true,
+            //         assetsSort: '',
+            //         builtAt: true,
+            //         cached: true,
+            //         cachedAssets: true,
+            //         children: true,
+            //         chunkModules: true,
+            //         chunkOrigins: true,
+            //         chunks: true,
+            //         chunksSort: true,
+            //         // context: ''
+            //         depth: true,
+            //         entrypoints: true,
+            //         env: true,
+            //         errorDetails: true,
+            //         errors: true,
+            //         // exclude: '',
+            //         // excludeAssets: true,
+            //         // excludeModules: true,
+            //         hash: true,
+            //         // maxModules: 0,
+            //         moduleTrace: true,
+            //         modules: true,
+            //         // modulesSort: '',
+            //         optimizationBailout: true,
+            //         performance: true,
+            //         providedExports: true,
+            //         // publicPath: '',
+            //         reasons: true,
+            //         source: true,
+            //         timings: true,
+            //         usedExports: true,
+            //         version: true,
+            //         warnings: true,
+            //         // warningsFilter: '',
+            //     },
+            //     excludeAssets: null,
+            //     logLevel: 'info',
+            // }),
+        ],
 
-    devtool: 'source-map',
+        devtool: 'source-map',
 
-    stats: {
-        all: true,
-    },
+        stats: {
+            all: true,
+        },
 
-    module: {
-        rules: [
-            {
-                test: /.(ts|tsx)?$/,
-                loader: 'ts-loader',
-                include: [path.resolve(__dirname, 'src')],
-                exclude: [/node_modules/]
-            }
-        ]
-    },
-
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendors: {
-                    priority: -10,
-                    test: /[\\/]node_modules[\\/]/
+        module: {
+            rules: [
+                {
+                    test: /.(ts|tsx)?$/,
+                    loader: 'ts-loader',
+                    include: [path.resolve(__dirname, 'src')],
+                    exclude: [/node_modules/]
                 }
-            },
+            ]
+        },
 
-            chunks: 'async',
-            minChunks: 1,
-            minSize: 30000,
-            name: true
-        }
-    },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    vendors: {
+                        priority: -10,
+                        test: /[\\/]node_modules[\\/]/
+                    }
+                },
 
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
+                chunks: 'async',
+                minChunks: 1,
+                minSize: 30000,
+                name: true
+            }
+        },
 
-    target: 'node'
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js']
+        },
+
+        target: 'node'
+    }
 };
